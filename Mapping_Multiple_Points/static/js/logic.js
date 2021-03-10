d959bf20a27fbb64db7bfd07c872c049f1529b45
Ryan Whited <rwhited@gobasports.com>
2021-03-09 21:35:46 -0600

    Adding updated files

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,14 +2,25 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-L.circle([34.0522, -118.2437], {
-    radius: 300,
-    color: 'black',
-    fillColor: 'lightyellow',
-    fillOpacity: '0.75'
- }).addTo(map);
+// An array containing each city's location, state, and population.
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        lineweight: 4, 
+        color: 'orange',
+        fillColor: 'orange',
+        fillOpacity: '0.25'
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
 
 
 // We create the tile layer that will be the background of our map.
